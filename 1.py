6d2a9f4b0a9feaa716450bd00b8746d33835f090
ESMIKA BHATTARAI <esmikab@gmail.com>
2024-12-01 13:28:48 +0545

    first commit

---
+++ 1.py
@@ -0,0 +1,22 @@
+print("HELLO PYTHON")
+
+if 5<10:
+    print("TRUE")
+else:
+    print("FALSE")
+
+a = 10
+b = 55 
+sum = a + b
+print(sum)
+
+dic = { "Name":"ESMIKA",
+"Class":"Bachelor",
+"Age":"21"}
+print(dic)
+print(type(dic))
+
+a = 4
+b = 12
+div = b/a
+print(div)
